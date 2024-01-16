e0b8d3ffe6131689680b74d51a951a542d0709a2
yumin-jung <jungym887@naver.com>
2024-01-16 16:58:03 +0900

    feat: Set canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import styled from "styled-components";
+import { MainCanvas } from "./components/MAinCanvas";
+
 function App() {
   return (
-    <>
-      Hello Scroll Dancer
-    </>
+    <Wrapper>
+      <MainCanvas />
+    </Wrapper>
   )
 }
 
 export default App
+
+const Wrapper = styled.div`
+width: 100vw;
+height:100vh;
+overflow:hidden;
+`
